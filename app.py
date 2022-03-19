e54831e558ae3c09b569a633fd16d689ef1aaa82
Pete Lealiiee Jr <petelealiieejr4@utexas.edu>
2022-03-19 02:00:34 -0500

    needs reformating

--- app.py
+++ app.py
@@ -5,8 +5,17 @@ import xmltodict
 
 ### CONSTANTS
 epoch_url_str = 'https://nasa-public-data.s3.amazonaws.com/iss-coords/2022-02-13/ISS_OEM/ISS.OEM_J2K_EPH.xml'  
-sghtng_url_str = '' 
+sighting_url_str = 'https://nasa-public-data.s3.amazonaws.com/iss-coords/2022-02-13/ISS_sightings/XMLsightingData_citiesUSA10.xml' 
+#sighting_url_str = 'https://nasa-public-data.s3.amazonaws.com/iss-coords/2022-02-13/ISS_sightings/XMLsightingData_citiesINT01.xml'
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+
+### GLOBAL VARIABLES 
+iss_epoch_data = []
+iss_sighting_data = []
+readonce = False
+
 
 
 ### MISCELLANEOUS FUNCTIONS
@@ -23,6 +32,8 @@ def get_xml_data(url_str:str)->dict:
     data_dict = xmltodict.parse(data_resp.content)
     return data_dict
 
+
+
 ### USAGE INFOFORMATION FUNCTION
 @app.route('/', methods=['GET'])
 def usage_info():
@@ -31,6 +42,7 @@ def usage_info():
         ['',''],
         ['Informational and Management Routes:', ''],
         ['/', '(GET) Print Route Information'],
+        ['/load', '(POST) Loads/Overwrites Data from URL ISS sources'  ]
     ]
 
     pos_tab = [
@@ -54,6 +66,21 @@ def usage_info():
     
     return usage_str
 
+
+@app.route('/load', methods=['POST'])
+def read_data_from_url():
+    global iss_epoch_data
+    global iss_sighting_data
+    global readonce
+    iss_epoch_data = get_xml_data(epoch_url_str)['ndm']['oem']['body']['segment']['data']['stateVector']
+    iss_sighting_data = get_xml_data(sighting_url_str)['visible_passes']['visible_pass']
+    if not readonce:
+        readonce = True
+    return f'Data has been scraped from ISS positioning and sighting URL sources below: \n Positioning: {epoch_url_str} \n Sighting: {sighting_url_str} \n'
+    
+
+
+
 ### ISS POSITIONING DATA FUNCTIONS 
 @app.route('/epochs', methods=['GET'])
 def epochs():
@@ -62,12 +89,12 @@ def epochs():
 
     args:                                                                                                                                                                                            
         (none)                                                                                                                                 
-    returns:                                                                                                                                                                                         
+    returns:                                                                                                                                                                                       
         (jsonify-ed list): Jsonified List containing all Epochs in Posiition Data Set                                                                                                                    """
-
-    data = get_xml_data(epoch_url_str)['ndm']['oem']['body']['segment']['data']['stateVector']
+    if not readonce:
+        return 'Use /load route to load data before proceeding \n'
     epoch_vec = []
-    for x in data:
+    for x in iss_epoch_data:
         epoch_vec.append(x['EPOCH'])
     return jsonify(epoch_vec)
 
@@ -80,16 +107,176 @@ def epoch_state(epoch):
         epoch (str): String of Epoch obtained from route 
     returns:                                                                                                                                                                                                 (jsonify-ed list): Jsonified Dictionary of Positioning Information at input epoch                                                                                                                    (str): Error string stating that specified epoch was not found 
     """
-
-    data = get_xml_data(epoch_url_str)['ndm']['oem']['body']['segment']['data']['stateVector']
-    for x in data:
+    if not readonce:
+        return 'Use /load route to load data before proceeding \n'
+    for x in iss_epoch_data:
         if epoch == x['EPOCH']:
             return jsonify(x)
-    return 'NO MATCH FOR INPUT EPOCH KEY FOUND IN DATA SET'
+    return 'NO MATCH FOR INPUT EPOCH KEY FOUND IN DATA SET \n'
+
+
+
 
 ### SIGHTING DATA FUNCTIONS 
 
+@app.route('/countries', methods=['GET'])
+def countries():
+    """                                                                                                                                                                                              
+    Called to return all countries in the ISS sightings data set                                                                                                                                    
+    args:                                                                                                                                                                                            
+        (none)                                                                                                                                                                                       
+    returns:                                                                                                                                                                                         
+        (jsonify-ed list): Jsonified List containing all countries in sightings Data Set                                                                                       
+    """
+    if not readonce:
+        return 'Use /load route to load data before proceeding \n'
+    countries_vec = []
+    for x in iss_sighting_data:
+        if not (x['country'] in countries_vec):
+            countries_vec.append(x['country'])
+    return jsonify(countries_vec)
 
+
+@app.route('/countries/<country>',methods=['GET'])
+def country_sightings(country):
+    """                                                                                                                                                                                              
+    Called to return information about sightings in specified country in the ISS sighting data set                                                                                    
+                                                                                                                                                                                                     
+    args:                                                                                                                                                                                            
+        country (str): String of country obtained from route                                                                                                                                             returns:                                                                                                                                                                                      
+        (jsonify-ed list): Jsonified Dictionary of sighting information at input country                                                                                                          
+        (str): Error string stating that specified country was not found                                                                                                                               
+    """
+    if not readonce:
+        return 'Use /load route to load data before proceeding \n'
+    country_info_vec = []
+    for x in iss_sighting_data:
+        if country == x['country']:
+            country_info_vec.append(x)
+    if len(country_info_vec)>0:
+        return jsonify(country_info_vec)
+    else:
+        return 'NO MATCH FOR INPUT COUNTRY KEY FOUND IN DATA SET \n'
+
+@app.route('/countries/<country>/regions',methods=['GET'])
+def country_regions(country):
+    """                                                                                                                                                                                              
+    Called to return sighting regions in specified country in the ISS sighting data set                                                                                                    
+    args:                                                                                                                                                                                            
+        country (str): String of country obtained from route                                                                                                                                         
+    returns:                                                                                                                                                                                                 (jsonify-ed list): Jsonified list of sighting regions in input country                                                                                                                     
+        (str): Error string stating that specified country was not found                                                                                                                                 """
+    if not readonce:
+        return 'Use /load route to load data before proceeding \n'
+    country_info_vec = []
+    for x in iss_sighting_data:
+        if country == x['country']:
+            country_info_vec.append(x)
+    if len(country_info_vec)>0:
+        regions_list = []
+        for y in country_info_vec:
+            if not (y['region'] in regions_list):
+                regions_list.append(y['region'])
+        return jsonify(regions_list)
+    else:
+        return 'NO MATCH FOR INPUT COUNTRY KEY FOUND IN DATA SET \n'
+
+
+@app.route('/countries/<country>/regions/<region>',methods=['GET'])
+def country_region_info(country,region):
+    """                                                                                                                                                                                              
+    Called to return sighting info in specified country and region in the ISS sighting data set                                                                                                              
+    args:                                                                                                                                                                                            
+        country (str): String of country obtained from route
+        region (str): String of region obtained from route
+    returns:                                                                                                                                                                                         
+        (jsonify-ed dict): Jsonified dictionary for specified country-region                                                                                                                      
+        (str): Error string stating that specified country was not found                                                                                                                           
+    """
+    if not readonce:
+        return 'Use /load route to load data before proceeding \n'
+    country_info_vec = []
+    for x in iss_sighting_data:
+        if country == x['country']:
+            country_info_vec.append(x)
+    if len(country_info_vec)>0:
+        region_info_vec = []
+        for y in country_info_vec:
+            if y['region'] == region:
+                region_info_vec.append(y)
+        if len(region_info_vec)>0:
+            return jsonify(region_info_vec)
+        else:
+            return 'NO MATCH FOR INPUT REGION KEY FOUND IN COUNTRY DATA SET \n'
+        
+    else:
+        return 'NO MATCH FOR INPUT COUNTRY KEY FOUND IN DATA SET \n'
+
+
+@app.route('/countries/<country>/regions/<region>/cities',methods=['GET'])
+def country_region_cities(country,region):
+    """                                                                                                                                                                                              
+    Called to return the cities in the specified country and region in the ISS sighting data set                                                                                                     
+
+    args:
+        country (str): String of country obtained from route                                                                                                                                                 region (str): String of region obtained from route                                                                                                                                               returns:                                                                                                                                                                                         
+        (jsonify-ed list): Jsonified list of cities in specified country-region                                                                                                                              (str): Error string stating that specified country or region was not found                                                                                                                              
+    """
+    if not readonce:
+        return 'Use /load route to load data before proceeding \n'
+    country_info_vec = []
+    for x in iss_sighting_data:
+        if country == x['country']:
+            country_info_vec.append(x)
+    if len(country_info_vec)>0:
+        region_info_vec = []
+        for y in country_info_vec:
+            if y['region'] == region:
+                region_info_vec.append(y)
+        if len(region_info_vec)>0:
+            cities_list = []
+            for z in region_info_vec:
+                if not (z['city'] in cities_list):
+                    cities_list.append(z['city'])
+            return jsonify(cities_list)
+        else:
+            return 'NO MATCH FOR INPUT REGION KEY FOUND IN COUNTRY DATA SET \n'
+    else:
+        return 'NO MATCH FOR INPUT COUNTRY KEY FOUND IN DATA SET \n'
+
+
+@app.route('/countries/<country>/regions/<region>/cities/<city>',methods=['GET'])
+def country_region_city_info(country,region,city):
+    """                                                                                                                                                                                                                                                           Called to return the sighing data in the specified country-region-city in the ISS sighting data set                                                                                                                                                          args:                                                                                                                                                                                                                                                            country (str): String of country obtained from route                                                                                                                                                 
+         region (str): String of region obtained from route                                                                                                                                               
+    returns:                                                                                                    \
+        (jsonify-ed dict): Jsonified dictionary of sightings in specified country-region-city                                                                                                                              
+        (str): Error string stating that specified country, region, or city was not found                                                                                                                                                                                                                                    
+    """
+    if not readonce:
+        return 'Use /load route to load data before proceeding \n'
+    country_info_vec = []
+    for x in iss_sighting_data:
+        if country == x['country']:
+            country_info_vec.append(x)
+    if len(country_info_vec)>0:
+        region_info_vec = []
+        for y in country_info_vec:
+            if y['region'] == region:
+                region_info_vec.append(y)
+        if len(region_info_vec)>0:
+            cities_info_vec = []
+            for z in region_info_vec:
+                if z['city'] == city:
+                    cities_info_vec.append(z)
+            if len(cities_info_vec)>0:
+                return jsonify(cities_info_vec)
+            else:
+                return 'NO MATCH FOR INPUT CITY KEY FOUND IN COUNTRY-REGION DATA SET \n'
+        else:
+            return 'NO MATCH FOR INPUT REGION KEY FOUND IN COUNTRY DATA SET \n'
+    else:
+        return 'NO MATCH FOR INPUT COUNTRY KEY FOUND IN DATA SET \n'
 
 
 ### MAIN 
